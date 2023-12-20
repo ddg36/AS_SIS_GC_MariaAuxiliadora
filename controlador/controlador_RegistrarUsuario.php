3fdad7b5659089bfecd32c14c0eeed5890013397
deml <ddg36_@hotmail.com>
2023-12-20 08:21:12 -0500

    eliminar innecesarios archivos y actualizar login y registrar links

--- controlador/controlador_RegistrarUsuario.php
+++ controlador/controlador_RegistrarUsuario.php
@@ -1,41 +1,20 @@
 <?php
 include_once "controlador_FuncionesDb.php"; // Incluir el archivo de conexión a la base de datos
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Conexión a la base de datos
-    $con = conectar();
-
-    // Recibir datos del formulario
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $id_rol = $_POST['id_rol']; // Asegúrate de que este valor exista en la tabla roles
-
-    // Preparar la consulta para insertar datos en la tabla Usuarios
-    $stmt = $con->prepare("INSERT INTO Usuarios (nombre_usuario, correo_electronico, contrasena, id_rol) VALUES (?, ?, ?, ?)");
-
-    // Encriptar la contraseña
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    // Vincular los parámetros y ejecutar la consulta
-    $stmt->bind_param("sssi", $username, $email, $hashed_password, $id_rol);
-
-    if ($stmt->execute()) {
-        // Registro exitoso
-        echo "Registro exitoso";
-        // Aquí podrías redirigir a una página de éxito o mostrar un mensaje apropiado al usuario
+if (!empty($_POST["submit"])) {
+    if (empty($_POST['username']) and empty($_POST['email']) and empty($_POST['password'])) {
+        echo "Por favor, ingrese todos los datos requeridos";
     } else {
-        // Error al registrar
-        echo "Error al registrar el usuario: " . $con->error;
-        // Manejar el error de acuerdo a tu flujo de aplicación
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $sql=$conectar->query("SELECT * FROM Usuarios WHERE nombre_usuario='$username' AND contrasena='$password' AND correo_electronico='$email'");
+        if ($datos=$sql->fetch_object()) {
+            # code...
+            header("location:index.php");
+        }    else {
+            # code...
+            echo "Datos incorrectos";
+        }
     }
-
-    // Cerrar la conexión y liberar recursos
-    $stmt->close();
-    $con->close();
-} else {
-    // Si no es una solicitud POST, puedes redirigir o manejar el flujo de otra manera
-    echo "Acceso denegado";
-    // Manejar el acceso no autorizado de acuerdo a tu flujo de aplicación
 }
-?>
